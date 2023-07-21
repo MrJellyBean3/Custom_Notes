066f84d374e53219f2b6acb68ed855da7bc37e2c
MrJellyBean3 <ehlowe31@gmail.com>
2023-07-20 23:12:00 -0400

    Basic App

--- main.py
+++ main.py
@@ -1,108 +1,101 @@
-#This code will be a custom note taking application that will allow users to make highly customized notes
-#import wxpython
-
-
-text_box_setup=[[5,5],[300,300]]#x,y,width,height
-
+import json
 import wx
+
+CONFIG_FILE = "config.json"
+
+class TextBoxFrame(wx.MiniFrame):
+    def __init__(self, parent, pos=(200,200), size=(200,200), content="", title="Text Box"):
+        super().__init__(parent, pos=pos, size=size, style=wx.RESIZE_BORDER | wx.CAPTION | wx.CLOSE_BOX)
+        self.parent = parent
+
+        self.main_sizer = wx.BoxSizer(wx.VERTICAL)
+
+        # Add an editable title to the top of the text box
+        self.title_edit = wx.TextCtrl(self, value=title, style=wx.BORDER_NONE)
+        font = self.title_edit.GetFont()
+        font.PointSize += 3
+        font.Bold = True
+        self.title_edit.SetFont(font)
+        self.title_edit.SetBackgroundColour(wx.WHITE)  # Change the color here
+
+        self.main_sizer.Add(self.title_edit, 0, wx.ALL, 5)
+
+        self.text_box = wx.TextCtrl(self, value=content, style=wx.TE_MULTILINE)
+        self.main_sizer.Add(self.text_box, 1, wx.EXPAND)
+
+        # Add a delete button
+        delete_button = wx.Button(self, label="Delete")
+        delete_button.Bind(wx.EVT_BUTTON, self.on_delete)
+        self.main_sizer.Add(delete_button, 0, wx.ALL, 5)
+
+        self.SetSizer(self.main_sizer)
+
+    def on_delete(self, event):
+        self.parent.text_box_frames.remove(self)  # Remove this frame from parent's list
+        self.Destroy()  # Close and destroy this frame
 
 class MyFrame(wx.Frame):
     def __init__(self, parent):
         super().__init__(parent, title="My wxPython App")
-        
-        # Create a panel inside the frame
-        self.panel = wx.Panel(self)
-        
-        # Create a text box inside the panel
-        self.text_box = wx.TextCtrl(self.panel, style=wx.TE_MULTILINE)
-        
-        # Create a box sizer
-        self.sizer = wx.BoxSizer(wx.VERTICAL)
-        
-        # Add the text box to the sizer
-        self.sizer.Add(self.text_box, 1, wx.EXPAND)
-        
-        # Set the sizer for the panel
-        self.panel.SetSizer(self.sizer)
-        
 
-        #Create the panel object
-        #panel = wx.Panel(self)
+        self.SetSize(1920,1080)
+        self.SetPosition((0,0))
 
-        #Create the text box object
-        text_box = wx.TextCtrl(self.panel, style = wx.TE_MULTILINE)
+        self.text_box_frames = []
 
-
-
-
-        #Create the menu bar object
         menu_bar = wx.MenuBar()
-
-        #Create the file menu object
         file_menu = wx.Menu()
-
-        #Create the edit menu object
         edit_menu = wx.Menu()
 
-        #Create the help menu object
-        help_menu = wx.Menu()
+        make_new_textbox = edit_menu.Append(wx.ID_ANY, "New Text Box", "Create a new Text Box")
+        save_item = file_menu.Append(wx.ID_SAVE, "Save", "Save current configuration and text box contents")
+        exit_item = file_menu.Append(wx.ID_EXIT, "Exit", "Exit the program")
 
-        #Create the file menu items
-        new_file_item = file_menu.Append(wx.ID_NEW, "New File", "Create a new file")
-        open_file_item = file_menu.Append(wx.ID_OPEN, "Open File", "Open an existing file")
-        save_file_item = file_menu.Append(wx.ID_SAVE, "Save File", "Save the current file")
-        save_as_file_item = file_menu.Append(wx.ID_SAVEAS, "Save As File", "Save the current file as a new file")
-        exit_file_item = file_menu.Append(wx.ID_EXIT, "Exit", "Exit the program")
-
-        #Create the edit menu items
-        undo_edit_item = edit_menu.Append(wx.ID_UNDO, "Undo", "Undo the last action")
-        redo_edit_item = edit_menu.Append(wx.ID_REDO, "Redo", "Redo the last action")
-        cut_edit_item = edit_menu.Append(wx.ID_CUT, "Cut", "Cut the selected text")
-        copy_edit_item = edit_menu.Append(wx.ID_COPY, "Copy", "Copy the selected text")
-        paste_edit_item = edit_menu.Append(wx.ID_PASTE, "Paste", "Paste the selected text")
-        delete_edit_item = edit_menu.Append(wx.ID_DELETE, "Delete", "Delete the selected text")
-        select_all_edit_item = edit_menu.Append(wx.ID_SELECTALL, "Select All", "Select all text")
-
-        #Create the help menu items
-        about_help_item = help_menu.Append(wx.ID_ABOUT, "About", "About the program")
-
-        #Add the file menu to the menu bar
         menu_bar.Append(file_menu, "File")
-
-        #Add the edit menu to the menu bar
         menu_bar.Append(edit_menu, "Edit")
 
-        #Add the help menu to the menu bar
-        menu_bar.Append(help_menu, "Help")
-
-        #Set the menu bar
         self.SetMenuBar(menu_bar)
 
-        #Bind the menu items to their respective functions
-        self.Bind(wx.EVT_MENU, lambda event: self.Close(True), exit_file_item)
-        self.Bind(wx.EVT_MENU, lambda event: wx.MessageBox("This is a custom note taking application that will allow users to make highly customized notes", "About", wx.OK), about_help_item)
+        self.Bind(wx.EVT_MENU, lambda event: self.Close(True), exit_item)
+        self.Bind(wx.EVT_MENU, self.new_textbox, make_new_textbox)
+        self.Bind(wx.EVT_MENU, self.save, save_item)
 
-
-
-
-
+        self.load()
         self.Show()
 
-#Create the application object
+    def new_textbox(self, event):
+        frame = TextBoxFrame(self)
+        self.text_box_frames.append(frame)
+        frame.Show()
+
+    def save(self, event):
+        config = {"text_boxes": []}
+        for child in self.GetChildren():
+            if isinstance(child, TextBoxFrame):
+                box_config = {
+                    "position": child.GetPosition().Get(),
+                    "size": child.GetSize().Get(),
+                    "content": child.text_box.GetValue(),
+                    "title": child.title_edit.GetValue()
+                }
+                config["text_boxes"].append(box_config)
+
+        with open(CONFIG_FILE, "w") as file:
+            json.dump(config, file)
+
+
+    def load(self):
+        try:
+            with open("config.json", "r") as file:
+                config = json.load(file)
+
+            for box in config["text_boxes"]:
+                frame = TextBoxFrame(self, box["position"], box["size"], box["content"], box["title"])  # Load the title here
+                frame.Show()
+        except FileNotFoundError:
+            pass
+
+
 app = wx.App()
-#Create the frame object
-frame = MyFrame(parent=None)
-
-#start app
+frame = MyFrame(None)
 app.MainLoop()
-
-
-
-
-
-
-
-
-
-#display finished message
-print("finished")
