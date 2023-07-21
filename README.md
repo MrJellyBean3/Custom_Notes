343ee997466ff294065219817e30a6dd4e58ccbd
MrJellyBean3 <ehlowe31@gmail.com>
2023-07-20 21:45:21 -0400

    Update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Custom_Notes
+#This program is meant to be a more dynamic and adjustable notepad for better note-taking efficiency
--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 #This code will be a custom note taking application that will allow users to make highly customized notes
 import wxpython
+
+print("Hello World")
